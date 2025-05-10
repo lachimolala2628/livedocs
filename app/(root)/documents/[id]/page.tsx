e68e3a5b6506e7ff5312a9a0485ba1e665931acb
Ayush Kumar <aayushkumar1228@gmail.com>
2025-05-11 03:16:33 +0530

    Editor & Home Page completing soon

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import CollaborativeRoom from '@/components/CollaborativeRoom'
 
 const Documents = () => {
     return (
-        <div>
-            Hello world
-        </div>
+        <main className='flex w-full flex-col items-center'>
+            <CollaborativeRoom />
+        </main>
     )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import Header from '@/components/Header'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import Image from 'next/image';
+import AddDocumentBtn from '@/components/AddDocumentBtn';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-const Home = () => {
+const Home = async () => {
+
+  const clerkUser = await currentUser();
+
+  if (!clerkUser) redirect('/sign-in');
+
+  const documents = [];
+
   return (
-    <div>Home</div>
+    <main className='home-container'>
+      <Header className='sticky top-0 left-0'>
+        <div className='flex items-center gap-2 lg:gap-4'>
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {
+        documents.length > 0 ? (
+          <div>
+
+          </div>
+        ) : (
+          <div className='document-list-empty'>
+            <Image
+              src='/assets/icons/doc.svg'
+              alt='document'
+              height={40}
+              width={40}
+              className='mx-auto'
+            />
+            <AddDocumentBtn 
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+        )
+      }
+    </main>
   )
 }
 
