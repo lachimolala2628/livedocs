3fe08352b4c634ab147f138deddb15d2f785738a
Ayush Kumar <aayushkumar1228@gmail.com>
2025-05-11 02:32:11 +0530

    authentication complete & working on Editor Page

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,16 +1,9 @@
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
 import React from 'react'
 
 const Documents = () => {
     return (
         <div>
-            <Header>
-                <div className='flex items-center w-fit justify-center gap-2'>
-                    <p className='document-title'>This a document title</p>
-                </div>
-            </Header>
-            <Editor />
+            Hello world
         </div>
     )
 }
